aa9368d23631bfcf4035e503cb4aeb495eafeb13
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-10-25 19:23:34 -0400

    perf: Refactor App component to include Player components and game board with two-way binding for player name and active player state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
+import { useState } from 'react';
 import Player from './components/Player';
+import GameBoard from './components/GameBoard';
 
 const App = () => {
+	// In cases like this that we have to specify which player is active for both GameBoard and Player components, since in some point they both need to change the state of the active player, we should add a useState hook in the closest parent of both.
+	const [activePlayer, setActivePlayer] = useState('X');
+
+	const handleSelectSquare = () => {
+		setActivePlayer((currActivePlayer) =>
+			currActivePlayer === 'X' ? 'O' : 'X'
+		);
+	};
 	return (
 		<main>
 			<div id="game-container">
-				<ol id="players">
-					<Player name="Player 1" symbol="X" />
-					<Player name="Player 2" symbol="O" />
+				<ol id="players" className="highlight-player">
+					<Player
+						initialName="Player 1"
+						symbol="X"
+						isActive={activePlayer === 'X'}
+					/>
+					<Player
+						initialName="Player 2"
+						symbol="O"
+						isActive={activePlayer === 'O'}
+					/>
 				</ol>
-				Game Board
+				<GameBoard
+					onSelectSquare={handleSelectSquare}
+					activePlayerSymbol={activePlayer}
+				/>
 			</div>
 		</main>
 	);
