2c9a6e2b48a6002202a10f51d94c1fdac98cede7
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-10-25 19:23:18 -0400

    feat: Refactor Player component to implement two-way binding for player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,34 @@
 import { useState } from 'react';
 
-const Player = ({ name, symbol }) => {
+const Player = ({ initialName, symbol, isActive }) => {
 	const [isEditing, setIsEditing] = useState(false);
-	let playerName = <span className="player-name">{name}</span>;
-	let editingPlayerName = (
-		<input type="text" name="editing-player-name" required />
-	);
+	const [playerName, setPlayerName] = useState(initialName);
 
 	const handleEditClick = () => {
-		setIsEditing(true);
+		// The editing argument is the state that we were in, then we change it to the curren state.
+		// Its a short way to say if the value of isEditing is true turn it to false and vise versa.
+		// Then we can use it to change the button from edit to save and otherwise.
+		// This kind of changing the state will guarantee that we will always get the latest state's update
+		setIsEditing((editing) => !editing);
 	};
 
+	const handleChange = (event) => {
+		setPlayerName(event.target.value);
+	};
+
+	let initialPlayerName = <span className="player-name">{playerName}</span>;
+	// Implementing a two way binding
+	let editingPlayerName = (
+		<input type="text" value={playerName} required onChange={handleChange} />
+	);
+
 	return (
-		<li>
+		<li className={isActive ? 'active' : undefined}>
 			<span className="player">
-				{!isEditing ? playerName : editingPlayerName}
+				{!isEditing ? initialPlayerName : editingPlayerName}
 				<span className="player-symbol">{symbol}</span>
 			</span>
-			<button onClick={handleEditClick}>Edit</button>
+			<button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
 		</li>
 	);
 };
