946ca520c89068b31b571f34570d22ec4293095b
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-10-28 16:40:53 -0400

    feat: Implement two-way binding for player name in Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react';
 import Player from './components/Player';
 import GameBoard from './components/GameBoard';
+import Log from './components/log';
 
 const App = () => {
 	// In cases like this that we have to specify which player is active for both GameBoard and Player components, since in some point they both need to change the state of the active player, we should add a useState hook in the closest parent of both.
 	const [activePlayer, setActivePlayer] = useState('X');
+	const [gameTurns, setGameTurns] = useState([]);
 
-	const handleSelectSquare = () => {
+	const handleSelectSquare = (rowIdx, colIdx) => {
 		setActivePlayer((currActivePlayer) =>
 			currActivePlayer === 'X' ? 'O' : 'X'
 		);
+		setGameTurns((prevTurns) => {
+			let currentPlayer = 'X';
+			if (currentPlayer.length > 0 && prevTurns[0].player === 'X') {
+				currentPlayer = 'O';
+			}
+
+			const updatedTurns = [
+				{ square: { row: rowIdx, col: colIdx }, player: currentPlayer },
+				...prevTurns,
+			];
+
+			return updatedTurns;
+		});
 	};
 	return (
 		<main>
@@ -26,11 +41,9 @@ const App = () => {
 						isActive={activePlayer === 'O'}
 					/>
 				</ol>
-				<GameBoard
-					onSelectSquare={handleSelectSquare}
-					activePlayerSymbol={activePlayer}
-				/>
+				<GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
 			</div>
+			<Log turns={gameTurns} />
 		</main>
 	);
 };
